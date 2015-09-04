788760aabb47d4617384e47ec5c43acc09a7cdaf
liviu.ignat <liviu.ignat@check24.de>
2015-09-04 17:12:20 +0200

    Add project

---
+++ Ch04_PacktLibrary/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Packt.LearningCS
+{    
+    public class Person
+    {
+        public string Name;
+        public DateTime DateOfBirth;
+    }
+}
---
+++ Ch04_PeopleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Packt.LearningCS;
+using static System.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ch04_PeopleApp
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var p1 = new Person();
+            WriteLine(p1.ToString());
+        }
+    }
+}
